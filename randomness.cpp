777e110e5eb0dc06184158ac0d959bd04b99f150
Awaknead <m13636311424@163.com>
2023-05-23 16:23:57 +0800

    c/c++ prime

--- randomness.cpp
+++ randomness.cpp
@@ -4,30 +4,66 @@
 #include<windows.h>
 #include<time.h>
 //随机号码--单色号
-int main()
+void excel ()
 {
-    int stret1,stret2,stret3;
-    // srand(time(NULL));//生成随机值
-    // stret1=rand()%30+1;//输出随机值
-    for(int j=0;j<=10;j++)
+    int status1,test1;
+    int arr[7]={0};
+    for(int i=0; i<6; i++)//总的打印行数 
     {
-        for(int i=0;i<=5;)//循环5个值
+        srand(time(NULL));
+        for(int j=0; j<6;)//一行有7个元素 
         {
-            srand(time(NULL));//生成随机值
-            stret1=rand()%30+2;//输出随机值
-            if(stret1 != stret2)//防止重复出现
+            Sleep(rand()%1010+9);
+            status1= rand()%30+3;//每个随机值赋值给数组
+            for(int x=0;x<6;x++)//判断每个元素是否重复
+            { 
+                if(arr[x] != status1)
+                {
+                    test1++;//判断是否递增5次
+                }
+            }
+            if(test1 == 6)//没有重复
             {
-                stret2=stret1;//没有重复-》赋值并输出
-                printf("--%d",stret2);
-                i++;  //输出5个值
+                arr[j] = status1;//数组赋值
+                j++;
+                test1 = 0;       //test1 归零
+            }
+            else
+            {
+                test1 = 0;
             }
         }
-        Sleep(1678);
-        srand(time(NULL));//生成随机值
-        stret3=rand()%30;
-        printf("    %d\n",stret3);
-        Sleep(1119);
+        Sleep(rand()%1060+5);
+        arr[6]= rand() %10+6;//单独蓝号
+        for(int y=0;y<7;y++)
+        {
+            printf("%d ",arr[y]);
+            arr[y] = 0;
+            if(y == 5)
+              putchar('-');
+        }
+        putchar('\n');
     }
+}
+int main()
+{
+    int value;
+    do
+    {
+        printf("****1. start ******\n");
+        printf("****0. quit  ******\n");
+        printf("plese>:");
+        scanf("%d",&value);
+        putchar('\n');
+        switch (value)
+        {
+            case 1: excel();
+                break;
+    
+            default:
+                break;
+        }
+    } while (value != 0);
     system("pause");
     return 0;
 }
